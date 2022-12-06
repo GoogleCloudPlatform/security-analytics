cc1c0bdd009c5b12957d50e38b98bd6b197c39fe
charlesbaer <30347307+charlesbaer@users.noreply.github.com>
2022-12-06 13:16:17 -0500

    Update 5_02_users_who_accessed_most_amount_of_data.sql

--- backends/log_analytics/sql/5_02_users_who_accessed_most_amount_of_data.sql
+++ backends/log_analytics/sql/5_02_users_who_accessed_most_amount_of_data.sql
@@ -14,15 +14,16 @@
  * limitations under the License.
  */
 
+
 SELECT
   proto_payload.audit_log.authentication_info.principal_email,
-  FORMAT('%9.2f',SUM(CAST(JSON_VALUE(proto_payload.audit_log.metadata,
-      "$.jobChange.job.jobStats.queryStats.totalBilledBytes") AS INT64))/POWER(2, 40)) AS Billed_TB
+  FORMAT('%9.2f',SUM(CAST(JSON_VALUE(
+    proto_payload.audit_log.metadata.jobChange.job.jobStats.queryStats.totalBilledBytes) AS INT64))/POWER(2, 40)) AS Billed_TB
 FROM
   `[MY_PROJECT_ID].[MY_DATASET_ID]._AllLogs`
 WHERE
   timestamp >= TIMESTAMP_SUB(CURRENT_TIMESTAMP(), INTERVAL 7 DAY)
-  AND JSON_VALUE(proto_payload.audit_log.metadata, "$.jobChange.job.jobConfig.type") = "QUERY"
-  AND log_name LIKE "%cloudaudit.googleapis.com%2Fdata_access"
+  AND JSON_VALUE(proto_payload.audit_log.metadata.jobChange.job.jobConfig.type) = "QUERY"
+  AND log_id = "cloudaudit.googleapis.com/data_access"
 GROUP BY principal_email
 ORDER BY Billed_TB DESC
