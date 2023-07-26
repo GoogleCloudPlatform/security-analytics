fcc1824015e95f4fc6008111da5926a9fc8f6267
Roy Arsan <rarsan@google.com>
2023-07-26 12:39:48 -0500

    Restrict results of CSA 4.10-4.11 in Log Analytics to Admin Activity actions only

--- backends/log_analytics/sql/4_11_autoscaling_usage_frequency_by_day.sql
+++ backends/log_analytics/sql/4_11_autoscaling_usage_frequency_by_day.sql
@@ -22,8 +22,8 @@ FROM
    `[MY_PROJECT_ID].[MY_LOG_BUCKET_REGION].[MY_LOG_BUCKET_NAME]._AllLogs`
 WHERE
   resource.type = "gce_instance_group_manager"
-  AND timestamp >= TIMESTAMP_SUB(CURRENT_TIMESTAMP(), INTERVAL 30 DAY)
   AND log_id = "cloudaudit.googleapis.com/activity"
+  AND timestamp >= TIMESTAMP_SUB(CURRENT_TIMESTAMP(), INTERVAL 30 DAY)
 GROUP BY
   1, 2
 ORDER BY
