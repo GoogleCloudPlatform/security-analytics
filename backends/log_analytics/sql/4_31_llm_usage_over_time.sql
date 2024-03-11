021998abe04acdeac0803759fc512c28a5931457
Roy Arsan <rarsan@google.com>
2024-03-11 22:13:27 +0000

    Add sample query to chart LLM usage over time

---
+++ backends/log_analytics/sql/4_31_llm_usage_over_time.sql
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2024 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+ /*
+ * ||||||||||||||||||||||||||||||||||||||
+ *   Log Analytics chart configuration:
+ * ||||||||||||||||||||||||||||||||||||||
+ * - Chart type: Bar chart - Vertical
+ * - Dimension (x-axis): timestamp
+ * - Measure (y-axis): Count rows (default)
+ * - Breakdown: resource_name 
+ */
+
+SELECT
+  timestamp,
+  proto_payload.audit_log.resource_name,
+  proto_payload.audit_log.authentication_info.principal_email
+FROM
+  `[MY_PROJECT_ID].[MY_LOG_BUCKET_REGION].[MY_LOG_BUCKET_NAME]._AllLogs`
+WHERE
+  -- timestamp >= TIMESTAMP_SUB(CURRENT_TIMESTAMP(), INTERVAL 7 DAY) AND
+  proto_payload.audit_log.service_name = "aiplatform.googleapis.com" AND
+  SPLIT(proto_payload.audit_log.method_name, '.')[SAFE_OFFSET(5)] IN ("Predict", "GenerateContent")
+LIMIT 10000
